458918f2f89e953bed2b4c8aeadc31ca66edbfbf
Ash Smith <ash.smith@soton.ac.uk>
2025-01-14 23:20:51 +0000

    Changed icon, this one is a bit more representative

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -7,7 +7,7 @@ class Migration(migrations.Migration):
     dependencies = [
     ]
 
-    forward = "INSERT INTO plugins (pluginid, name, icon, component, componentname, config, slug, sortorder) VALUES ('9bb5db91-9527-49fb-9299-9621eea2742d', '{\"en\": \"Curator\"}', 'fa fa-list-alt', 'views/components/plugins/curator', 'curator', '{\"show\": true, \"is_workflow\": false, \"description\": \"\"}', 'curator', '1' );"
+    forward = "INSERT INTO plugins (pluginid, name, icon, component, componentname, config, slug, sortorder) VALUES ('9bb5db91-9527-49fb-9299-9621eea2742d', '{\"en\": \"Curator\"}', 'fa fa-pencil', 'views/components/plugins/curator', 'curator', '{\"show\": true, \"is_workflow\": false, \"description\": \"\"}', 'curator', '1' );"
 
     reverse = "DELETE FROM plugins where pluginid = '9bb5db91-9527-49fb-9299-9621eea2742d';"
 
