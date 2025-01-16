4ec2edf7d77f035702ac98abef2e66372bfe8339
Ash Smith <ash.smith@soton.ac.uk>
2025-01-16 15:42:08 +0000

    Creates objects on web call

--- views/curator.py
+++ views/curator.py
@@ -1,29 +1,53 @@
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
-from arches.app.models.models import ResourceInstance, TileModel
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, Http404
+from uuid import uuid4
 from ..models import CuratedDataset
+import json
 
 @method_decorator(csrf_exempt, name="dispatch")
 class Curator(View):
 
 	def serialize_datasets(self, user_id):
-		res = [{'id': str(dataset.search_id), 'label': dataset.search_label, 'results': dataset.search_results_count} for dataset in CuratedDataset.objects.filter(search_user__id=user_id)]
+		res = [{'id': str(dataset.search_id), 'label': dataset.search_label, 'results': dataset.search_results_count} for dataset in CuratedDataset.objects.filter(search_user__id=user_id).exclude(search_label='')]
 		return res
+
+	def tidy_up(self):
+		return 0
 
 	def get(self, request):
 		user = request.user
 		if not user.is_authenticated:
 			return JsonResponse({"datasets": []})
-		data = {"records": self.serialize_datasets(user.id)}
+		data = {"datasets": self.serialize_datasets(user.id)}
 		return JsonResponse(data)
 
 	def post(self, request):
-		state = {
-			"resources": ResourceInstance.objects.count(),
-			"tiles": TileModel.objects.count()
-		}
-		data = {"records": self.serialize_datasets(0)}
+		user = request.user
+		if not user.is_authenticated:
+			return JsonResponse({"id": "", "datasets": []})
+		request_data = json.loads(request.body)
+		if len(request_data['id']) == 0:
+			ret = CuratedDataset(search_user=user, search_id=uuid4(), search_results_count=0)
+		else:
+			ret = get_object_or_404(CuratedDataset, search_id=request_data['id'])
+		if user.id != ret.search_user.id:
+			raise Http404()
+		if 'title' in request_data:
+			if len(request_data['title']) > 0:
+				ret.search_label = request_data['title']
+		if 'results' in request_data:
+			ret.search_results = int(request_data['results'])
+		if 'url' in request_data:
+			if len(request_data['url']) > 0:
+				ret.search_url = request_data['url']
+		if 'geojson' in request_data:
+			if len(request_data['geojson']) > 0:
+				ret.search_results = {"search_url": request_data['geojson'], "features": [], "type": "FeatureCollection"}
+		ret.save()
+
+		data = {"id": ret.search_id, "datasets": self.serialize_datasets(user.id)}
 		return JsonResponse(data)
 
